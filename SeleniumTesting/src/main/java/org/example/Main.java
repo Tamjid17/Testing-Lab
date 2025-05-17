acc7ac34cd3d5f55eb0a35ee7fbef2297d74a1df
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-18 05:00:48 +0600

    Add selenium testing dependencies and basic selenium testing code

--- SeleniumTesting/src/main/java/org/example/Main.java
+++ SeleniumTesting/src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Browser Driver\\chromedriver.exe");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.close();
     }
 }
